a5107f07bc4220a040e776fbbe71cb36cab68e24
chrisi.gusenbauer101@gmail.com <chrisi.gusenbauer101@gmail.com>
2020-03-31 12:23:28 +0200

    gui

---
+++ src/test/java/at/jku/timemanagerapp/AppTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.jku.timemanagerapp;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ *
+ * @author chris
+ */
+public class AppTest {
+    
+    
+    @Test
+    public void start() {
+    }
+
+    @Test
+    public void setRoot() {
+    }
+
+    @Test
+    public void main() {
+    }
+    
+}
